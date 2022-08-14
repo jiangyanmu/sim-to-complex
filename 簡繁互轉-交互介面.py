b2454e0208b9767aac0ed9b22e4b28bcaa1d7251
jiangyanmu <105329293+jiangyanmu@users.noreply.github.com>
2022-08-14 14:40:49 +0800

    Update 簡繁互轉-交互介面.py

--- 簡繁互轉-交互介面.py
+++ 簡繁互轉-交互介面.py
@@ -6,12 +6,12 @@ from rich.progress import track
 import docx
 
 root = tk.Tk()
-root.title('简<->繁 转换器')    #标题
-root.geometry('+600+250')      #视窗位置
+root.title('簡<->繁 轉換器')    #標題
+root.geometry('+600+250')      #視窗位置
 canvas1 = tk.Canvas(root,width=300,height=250)
 canvas1.pack()
 
-name = tk.Label(root,text='简<->繁 转换器')
+name = tk.Label(root,text='簡<->繁 轉換器')
 name.config(font=20)
 canvas1.create_window(150,60,window=name)
 
@@ -26,18 +26,18 @@ class dif_type():
         return txt_file
 
     def docx_type(self):
-        word = docx.Document(self.file)  #读取docx文件
+        word = docx.Document(self.file)  #讀取docx文件
 
         data = []
-        for paragraph in word.paragraphs:   #将内容储存于list中
+        for paragraph in word.paragraphs:   #將內容處存於list中
             data.append(paragraph.text)
 
-        docx_file = pd.DataFrame()   #将list存于dataframe中
+        docx_file = pd.DataFrame()   #將list存于dataframe中
         docx_file[''] = pd.Series(data)
 
         return docx_file
 
-# 获取文件
+# 獲取文件
 def get_files():
     global convert_file
     file = filedialog.askopenfilename()
@@ -49,52 +49,50 @@ def get_files():
         convert_file = final_type.docx_type()
 
 
-# get_file加（）—— get_file()，在运行时会直接启动
-get_button = tk.Button(text='选取文件',command=get_files,font=16)
+get_button = tk.Button(text='選取文件',command=get_files,font=16)
 canvas1.create_window(150,130,window=get_button)
 
-# 文件格式(副档名)
+# 文件格式(副檔名)
 file_format = [('txt files','*.txt')]
 
 
 def convert_complex():
     global convert_file
     data = []
-    for i in track(range(convert_file.shape[0]),description='转换中:'):
+    for i in track(range(convert_file.shape[0]),description='轉換中:'):
         open_cc = OpenCC('s2t.json')
         result = open_cc.convert(convert_file.loc[i][0])
-        #不加[0]会显示格式错误（因为只有[i]会返回pandas文件返回数据格式）
 
         data.append(result)
 
     # 建立新的 dataframe
     new_file = pd.DataFrame()
-    new_file['繁体文件：'] = pd.Series(data)
+    new_file['繁體文件：'] = pd.Series(data)
 
     # 保存文件
     import_file = filedialog.asksaveasfilename(filetypes=file_format)
     new_file.to_csv(import_file,line_terminator='\n\n',header=False,index=False)
 
-convert_button = tk.Button(text='简->繁',command=convert_complex,font=16)
+convert_button = tk.Button(text='簡->繁',command=convert_complex,font=16)
 canvas1.create_window(90,190,window=convert_button)
 
 def convert_simple():
     global convert_file
     data = []
-    for i in track(range(convert_file.shape[0]),description='转换中:'):
+    for i in track(range(convert_file.shape[0]),description='轉換中:'):
         open_cc = OpenCC('t2s.json')
-        result = open_cc.convert(convert_file.loc[i][0]) #不加[0]会显示格式错误（因为只有[i]会返回pandas文件返回样式）
+        result = open_cc.convert(convert_file.loc[i][0])
         data.append(result)
 
     # 建立新的 dataframe
     new_file = pd.DataFrame()
-    new_file['简体文件：'] = pd.Series(data)
+    new_file['簡體文件：'] = pd.Series(data)
 
     # 保存文件
     import_file = filedialog.asksaveasfilename(filetypes=file_format)
     new_file.to_csv(import_file,line_terminator='\n\n',header=False,index=False,encoding='utf-8-sig')
 
-convert_button = tk.Button(text='繁->简',command=convert_simple,font=12)
+convert_button = tk.Button(text='繁->簡',command=convert_simple,font=12)
 canvas1.create_window(210,190,window=convert_button)
 
 root.mainloop()
